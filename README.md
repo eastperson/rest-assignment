8087b41af89892dfdf57dd4cc04d821d42b99570
EP <kjuioqq8@gmail.com>
2021-04-17 13:29:28 +0900

    과제 셋팅 완료

---
+++ README.md
@@ -0,0 +1,7 @@
+<h1>restapi-assignment</h1>
+<hr>
+문제
+
+![캡처](https://user-images.githubusercontent.com/66561524/115101552-26c4e400-9f80-11eb-84d8-3411b4f0cb72.PNG)
+
+
---
+++ src/main/java/com/ex/rest/controller/UserController.java
@@ -0,0 +1,18 @@
+package com.ex.rest.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/user")
+public class UserController {
+
+    // 1. 회원가입
+
+    // 2. 로그인, 토큰 반환
+
+    // 3. 내 정보 확인
+
+
+}
